6fb5b5c770bf303317c477cc884f93431c37c192
Alexandr <kotte48@gmail.com>
2023-10-28 18:49:07 +0300

    Настройка gulp и package.json + Автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,141 @@
-import gulp from 'gulp';
-import plumber from 'gulp-plumber';
-import sass from 'gulp-dart-sass';
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
-import browser from 'browser-sync';
+// Импорт модулей
+import gulp from "gulp";
+import plumber from "gulp-plumber";
+import sass from "gulp-dart-sass";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import csso from "postcss-csso";
+import rename from "gulp-rename";
+import htmlmin from "gulp-htmlmin";
+import terser from "gulp-terser";
+import webp from "gulp-webp";
+import imagemin, { gifsicle, mozjpeg, optipng, svgo } from "gulp-imagemin";
+import svgstore from "gulp-svgstore";
+import browser from "browser-sync";
+import del from "del";
 
 // Styles
-
 export const styles = () => {
-  return gulp.src('source/sass/style.scss', { sourcemaps: true })
+  return gulp
+    .src("source/sass/style.scss", { sourcemaps: true })
     .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([autoprefixer(), csso()]))
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("source/css", { sourcemaps: "." }))
+    .pipe(gulp.dest("build/css"))
     .pipe(browser.stream());
-}
+};
+
+// Минимизатор HTML файлов
+export const html = () => {
+  return gulp
+    .src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
+};
+
+// Минимизатор скриптов
+export const scripts = () => {
+  return gulp
+    .src("source/js/script.js")
+    .pipe(terser())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(gulp.dest("source/js"));
+};
+
+// Копирование СЖАТЫХ изображений
+export const optimizeImages = () => {
+  return gulp
+    .src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin())
+    .pipe(gulp.dest("build/img"));
+};
+
+// Копирование НЕ СЖАТЫХ изображений
+export const copyImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}").pipe(gulp.dest("build/img"));
+};
+
+// Создание WebP изображений
+export const createWebp = () => {
+  return gulp
+    .src("source/img/**/*.{jpg,png}")
+    .pipe(webp({ quality: 70 }))
+    .pipe(gulp.dest("source/img"))
+    .pipe(gulp.dest("build/img"));
+};
+
+// Работа со Sprite
+export const sprite = () => {
+  return gulp
+    .src("source/img/socials/*.svg")
+    .pipe(
+      svgstore({
+        inlineSvg: true,
+      })
+    )
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img/sprites"))
+    .pipe(gulp.dest("source/img/sprites"));
+};
+
+// Copy
+export const copy = (done) => {
+  gulp
+    .src(
+      [
+        "source/fonts/*.{woff2,woff}",
+        "source/*.ico",
+        "source/img/**/*.svg",
+        "!source/img/icons/*.svg",
+      ],
+      {
+        base: "source",
+      }
+    )
+    .pipe(gulp.dest("build"));
+  done();
+};
+
+// Удаление готовой сборки
+export const clean = () => {
+  return del("build");
+};
 
 // Server
-
-const server = (done) => {
+export const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: "source",
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-}
+};
 
 // Watcher
+export const watcher = () => {
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
+  gulp.watch("source/js/script.js", gulp.series(scripts));
+  gulp.watch("source/*.html").on("change", browser.reload);
+};
 
-const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
-}
-
+// Создание Build
+export const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(styles, html, scripts, sprite, createWebp)
+);
 
 export default gulp.series(
-  styles, server, watcher
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(styles, html, scripts, sprite, createWebp),
+  gulp.series(server, watcher)
 );
